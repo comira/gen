40824b22bcf396284ed7a56052c896cdf065c23c
Comira <57299183+comira@users.noreply.github.com>
2020-03-07 16:40:13 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+let fs = require('fs');
+const src = core.getInput('src');
+const dist = core.getInput('dist');
+
+console.log(src);
+console.log(dist);
+
+function base64_encode(file) {
+    let f = fs.readFileSync(file);
+    return f.toString('base64');
+}
+
+fs.readdirSync('/home/runner/work/').forEach(f => {
+    console.log(f);
+});
